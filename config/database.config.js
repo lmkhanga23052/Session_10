6567333b8b0d5926ab20f662b0b0a10d2eca3e96
lmkhanga23052 <lmkhanga23052@cusc.ctu.edu.vn>
2024-11-19 08:38:49 +0700

    second commit

--- config/database.config.js
+++ config/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://MongoAtlas:khang@cu$c@cluster0.ej82y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect('mongodb+srv://MongoAtlas:khang@cu$c@cluster0.mongodb.net/myDatabase?retryWrites=true&w=majority');
         console.log('Kết nối MongoDB thành công');
     } catch (err) {
         console.error('Lỗi kết nối MongoDB:', err);
